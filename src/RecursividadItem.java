c42a3c12094b5a8d983bed06456b979809332fe5
giticlon <1231111+giticlon@users.noreply.github.com>
2020-04-24 18:46:17 +0200

    Incluido_texto_cabecera

--- src/RecursividadItem.java
+++ src/RecursividadItem.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class RecursividadItem {
 /* Este método funciona. Utilizado en la PED de EPED
 	/*
+	 * 
+     * Método que completa a getDoctor(int id) para realizar la búsqueda de
+     * un doctor en la academia
+     *
+     * @param id identificador del Doctor a buscar
+     * @param doctor de la academia cuyo id se compara con el id recibido por
+     * parámetro hasta que se produce la coincidencia
+     * @param s va desde el tamaño de la academia hasta 0 para saber si hemos
+     * revisado todos los doctores de la acdemia
+     * @return el doctor de la academia que coincide con el id recibido por
+     * parámetro
+
 	private DoctorIF getDoctor(int id, DoctorIF doctor, int s) {
 
         if (s <= 0 || doctor.getID() == id) {
